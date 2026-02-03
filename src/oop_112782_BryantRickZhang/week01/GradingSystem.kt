2d895fc81c01e1318a1d69a51715e6e7faf0c5a9
bxy4nt <bryantrickzhang@gmail.com>
2026-02-03 20:39:23 +0700

    week01: refractor to use val and string templates

--- src/oop_112782_BryantRickZhang/week01/GradingSystem.kt
+++ src/oop_112782_BryantRickZhang/week01/GradingSystem.kt
@@ -1,4 +1,10 @@
 package oop_112782_BryantRickZhang.week01
 
-class GradingSystem {
+fun main(){
+    // REFRACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "Bryant Rick Zhang"
+    val score = 80
+
+    // REFRACTOR: String Template ($name)
+    println("Name: $name , Nilai: $score")
 }
