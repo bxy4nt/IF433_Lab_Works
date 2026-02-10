10e276709f81d197dfab418260809f888a295469
bxy4nt <bryantrickzhang@gmail.com>
2026-02-10 15:50:42 +0700

    week02: add warning validation in init block

--- src/oop_112782_BryantRickZhang/week02/Student.kt
+++ src/oop_112782_BryantRickZhang/week02/Student.kt
@@ -1,2 +1,14 @@
 package oop_112782_BryantRickZhang.week02
 
+class student (val name: String val nim: String  val major: String) {
+    init {
+        //Validasi Sederhana : Cek panjang NIM
+        if(nim.length !=5 ) {
+            println("Warning : Objek tercipta dengan NIM ($nim") yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem")
+        } else {
+            println("LOG: Objek Student $name berhasil dilokasikan di memory")
+        }
+    }
+}
+
