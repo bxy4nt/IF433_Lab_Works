eb553e24edcc13840a49451c2093d16a629020be
bxy4nt <bryantrickzhang@gmail.com>
2026-02-24 10:52:10 +0700

    week03: fix setter using backing field

--- src/oop_112782_BryantRickZhang/week03/employee.kt
+++ src/oop_112782_BryantRickZhang/week03/employee.kt
@@ -1,4 +1,14 @@
 package oop_112782_BryantRickZhang.week03
 
-class employee {
+class Employee(
+    val name: String,
+) {
+    var salary: Int = salary
+        set(value) {
+            if(value >=0){
+                field = value
+            } else {
+                println("Salary tidak boleh negatif!")
+            }
+        }
 }
---
+++ src/oop_112782_BryantRickZhang/week03/main.kt
@@ -0,0 +1,6 @@
+package oop_112782_BryantRickZhang.week03
+
+fun main() {
+    val e = Employee("Bryant", 1000)
+    e.salary = 5000
+}
