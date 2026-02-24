a5122cd6d3b27109b848b684604bdaf0e74cb899
bxy4nt <bryantrickzhang@gmail.com>
2026-02-24 11:22:27 +0700

    week03: test all encapsulation logic in main

--- src/oop_112782_BryantRickZhang/week03/employee.kt
+++ src/oop_112782_BryantRickZhang/week03/employee.kt
@@ -2,23 +2,27 @@ package oop_112782_BryantRickZhang.week03
 
 class Employee(
     val name: String,
+    salary: Int
 ) {
     var salary: Int = salary
         set(value) {
-            if(value >=0){
+            if (value >= 0) {
                 field = value
             } else {
                 println("Salary tidak boleh negatif!")
             }
         }
-}
 
     private var performance: Int = 0
 
-fun updateRating(newrating: Int){
+    fun updateRating(newrating: Int) {
     if(newrating in 1..5){
         performance = newrating
     } else {
         println("Rating harus antara 1 sampai 5")
     }
+}
+
+    val bonus: Int
+        get() = salary / 10
 }
--- src/oop_112782_BryantRickZhang/week03/main.kt
+++ src/oop_112782_BryantRickZhang/week03/main.kt
@@ -2,5 +2,11 @@ package oop_112782_BryantRickZhang.week03
 
 fun main() {
     val e = Employee("Bryant", 1000)
+    e.salary = -500
     e.salary = 5000
+
+    e.updateRating(4)
+
+    println("Salary: ${e.salary}")
+    println("Bonus: ${e.bonus}")
 }
