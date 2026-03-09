b0e5f8db581206d29db8bc56d5650acecb5a2329
bxy4nt <bryantrickzhang@gmail.com>
2026-03-09 20:04:42 +0700

    week05: create Dosen class inheriting Pegawai correction

--- src/oop_112782_BryantRickZhang/week05/Dosen.kt
+++ src/oop_112782_BryantRickZhang/week05/Dosen.kt
@@ -5,10 +5,10 @@ class Dosen(
 ) : Pegawai (nama) {
 
     override fun bekerja() {
-        println("$nama sedang mengajar mahasiswa.")
+        println("$nama sedang menyiapkan materi perkuliahan dan merivisi RPKPS.")
     }
 
     fun mengajar (){
-        println("$nama sedang menjelaskan materi dikelas!")
+        println("$nama sedang mengajar mahasiswa dikelas.")
     }
 }
---
+++ src/oop_112782_BryantRickZhang/week05/Main.kt
@@ -0,0 +1,7 @@
+package oop_112782_BryantRickZhang.week05
+
+fun Main() {
+
+    val dosen = Dosen("Pak Budi")
+    val admin = Admin("Bu Lina")
+}
