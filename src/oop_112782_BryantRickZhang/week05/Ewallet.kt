3a08cc64137e6c87781800db8d7a4e8ba4c8a3e5
bxy4nt <bryantrickzhang@gmail.com>
2026-03-09 20:37:50 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_112782_BryantRickZhang/week05/Ewallet.kt
@@ -0,0 +1,22 @@
+package oop_112782_BryantRickZhang.week05
+
+class Ewallet (
+    accountName: String,
+    var balance: Double
+) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil menggunakan EWallet oleh $accountName")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topup(amount: Double) {
+        balance += amount
+        println("Topup berhasil. Saldo sekarang: $balance")
+    }
+}
