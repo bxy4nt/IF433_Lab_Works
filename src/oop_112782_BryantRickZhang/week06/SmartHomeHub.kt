db7fe13e3fdab60a6fc820cbe54aa6e0352d2020
bxy4nt <bryantrickzhang@gmail.com>
2026-03-16 15:38:36 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_112782_BryantRickZhang/week06/SmartHomeHub.kt
@@ -0,0 +1,20 @@
+package oop_112782_BryantRickZhang.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+
+        for (device in devices) {
+
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
