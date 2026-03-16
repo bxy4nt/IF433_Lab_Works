92c11015c6ac9aecc69c51a0215f22e636c3e147
bxy4nt <bryantrickzhang@gmail.com>
2026-03-16 14:48:05 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_112782_BryantRickZhang/week06/GadgetInterface.kt
@@ -0,0 +1,15 @@
+package oop_112782_BryantRickZhang.week06
+
+interface Camera {
+
+    fun takePhoto() {
+        println("Mengambil foto...")
+    }
+}
+
+interface Phone {
+
+    fun takePhoto() {
+        println("Mengambil foto menggunakan phone...")
+    }
+}
