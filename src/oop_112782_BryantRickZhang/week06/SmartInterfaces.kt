d29187179ca3ead4bba2a508bc317b733e3cb795
bxy4nt <bryantrickzhang@gmail.com>
2026-03-16 15:05:35 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_112782_BryantRickZhang/week06/SmartInterfaces.kt
@@ -0,0 +1,22 @@
+package oop_112782_BryantRickZhang.week06
+
+interface SmartDevice {
+
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+
+    fun turnOn()
+
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
