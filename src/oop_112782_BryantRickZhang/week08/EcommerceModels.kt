f6f9dc07323e665adf3541c609c37c28a6f7b895
bxy4nt <bryantrickzhang@gmail.com>
2026-04-14 15:37:57 +0700

    week08: (task) create sealed class Product and data classes

--- src/oop_112782_BryantRickZhang/week08/EcommerceModels.kt
+++ src/oop_112782_BryantRickZhang/week08/EcommerceModels.kt
@@ -1,4 +1,15 @@
 package oop_112782_BryantRickZhang.week08
 
-class EcommerceModels {
-}
+sealed class Product
+
+data class Electronic(
+    val id : String,
+    val name : String,
+    val warrantyMonths : Int,
+) : Product()
+
+data class Clothing(
+    val id : String,
+    val name : String,
+    val size : String
+) : Product()
