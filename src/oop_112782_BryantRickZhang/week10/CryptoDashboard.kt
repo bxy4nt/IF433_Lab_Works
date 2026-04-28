95f226ba7c56fe58811e622809c7564f7d80fe00
bxy4nt <bryantrickzhang@gmail.com>
2026-04-28 16:13:40 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_112782_BryantRickZhang/week10/CryptoDashboard.kt
+++ src/oop_112782_BryantRickZhang/week10/CryptoDashboard.kt
@@ -13,4 +13,13 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin Name: ${coin.name}, Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX-991", 0.05))
+    txRepo.add(Transaction("TX-992", 150.0))
+
+    println("\n=== RECENT TRANSACTIONS ===")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id}, Amount: ${tx.amount}")
+    }
 }
