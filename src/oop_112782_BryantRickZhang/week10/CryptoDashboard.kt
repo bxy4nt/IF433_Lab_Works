4a5c7a3effd00e7c0f692e496de477c158492312
bxy4nt <bryantrickzhang@gmail.com>
2026-04-28 16:08:16 +0700

    week10: (task) populate coin repository

--- src/oop_112782_BryantRickZhang/week10/CryptoDashboard.kt
+++ src/oop_112782_BryantRickZhang/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_112782_BryantRickZhang.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
