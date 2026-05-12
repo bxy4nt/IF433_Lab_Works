00649c5c000ae45ddeb51827700b84685c91c874
bxy4nt <bryantrickzhang@gmail.com>
2026-05-12 13:52:37 +0700

    week12: (task) implement dispenseKibble function

--- src/oop_112782_BryantRickZhang/week11/week12latianaja.kt
+++ src/oop_112782_BryantRickZhang/week11/week12latianaja.kt
@@ -1,29 +1,61 @@
 package oop_112782_BryantRickZhang.week11
 
-fun pembagian () {
-    try {
-        val a = 10
-        val b = 2
+fun pembagian(){
+    try{
+        val a= 10
+        val b =2
         val hasil = a/b
-        println("hasil bagi $hasil")
+        println("Hasil bagi adalah ${hasil}")
     } catch (e: Exception) {
-        println("ada error " + e.message)
-    } finally {
-        println("selesai try catch")
+        println("Ada error " + e.message)
+    } finally{
+        println("Selesai try catch")
     }
 }
 
 fun cek_tipe_variabel(){
-    var angka:Int = try{
+    var angka: Int = try {
         Integer.parseInt("123")
-    } catch (e: Exception){
-        println("Ada error ${e.message}")
-        -67
+    } catch (e:Exception){
+        println("ada error ${e.message}")
+        -77
     }
     println(angka)
 }
 
+fun cek_nilai(nilai:Int){
+    if(nilai<0){
+        throw IllegalArgumentException("Masa nilai minus")
+    } else{
+        println("Nilai kamu adalah" + nilai)
+    }
+}
+
+class cek_saldo_rekening(val pengeluaran: Int, val saldo: Int):
+    Exception("Saldo kamu sisa $saldo, ga cukup bua belanja $pengeluaran")
+
+class transaksi_keuangan(val totalBelanja:Int){
+    fun narik_uang(balance: Int){
+        if(totalBelanja > balance){
+            throw cek_saldo_rekening(totalBelanja, balance)
+        } else {
+            println("Transaksi Berhasil, sisa saldo ${balance - totalBelanja}")
+        }
+    }
+
+}
+
 fun main(){
     pembagian()
+
     cek_tipe_variabel()
+    try {
+        cek_nilai(-10)
+    } catch(e: Exception){
+        println("Ada error di nilai: "+ e.message)
+    }
+
+    val trx = transaksi_keuangan(1000)
+    trx.narik_uang(1200)
+
 }
---
+++ src/oop_112782_BryantRickZhang/week12/SmartFeeder.kt
@@ -0,0 +1,24 @@
+package oop_112782_BryantRickZhang.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+
+    return availableGram - requestedGram
+}
